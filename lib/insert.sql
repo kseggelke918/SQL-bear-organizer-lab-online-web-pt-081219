844c047f7c23051e5832e8cb48e26c772c7d39b1
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-15 19:24:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Brown", "Gentle", );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL);
